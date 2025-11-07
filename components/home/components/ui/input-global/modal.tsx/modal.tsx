25c49a6b3a6dbea98da984442be8f74a343222dc
Laureano4656 <66977263+Laureano4656@users.noreply.github.com>
2025-11-06 21:53:09 -0300

    removing login button from menu, adding pomelo logo and decreasing border with of popups

--- components/home/components/ui/input-global/modal.tsx/modal.tsx
+++ components/home/components/ui/input-global/modal.tsx/modal.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react'
 
 const Modal = ({ children, setOpenModal, openModal }) => {
-  return (
-    <>
-      {openModal && (
-        <div
-          className="fixed top-0 left-0 z-10 flex items-center justify-center w-full h-screen bg-black bg-opacity-60"
-        //   onClick={() => setOpenModal(false)}
-        >
-          {/* w-[16vw] h-[25vh] */}
-          <div className="py-[10px] bg-white sm:w-96 w-10/12  h-[50vh] sm:h-[250px] rounded-[32px] flex flex-col justify-between items-center border-stone-950 border-4">
-            {children}
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
+	return (
+		<>
+			{openModal && (
+				<div
+					className='fixed top-0 left-0 z-10 flex items-center justify-center w-full h-screen bg-black bg-opacity-60'
+					//   onClick={() => setOpenModal(false)}
+				>
+					{/* w-[16vw] h-[25vh] */}
+					<div className='py-[10px] bg-white sm:w-96 w-10/12  h-[50vh] sm:h-[250px] rounded-[32px] flex flex-col justify-between items-center border-stone-950 border-2'>
+						{children}
+					</div>
+				</div>
+			)}
+		</>
+	)
+}
 
-export default Modal;
+export default Modal
